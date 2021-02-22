00b48145daf60a0966ad5b804c381887615355ba
jose queiroz <zemqueiroz@gmail.com>
2021-02-22 17:26:12 +0000

    finished coding dogs controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,7 +1,30 @@
 class DogsController < ApplicationController
-    def index
-        @dogs = Dog.all
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dogs = Dog.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @dog = Object.new(dog_params)
+    if @dog.save
+      flash[:success] = "Dog-entry successfully created"
+      redirect_to dogs_path(@dog)
+    else
+      flash[:error] = "Something went wrong"
+      render :new
     end
-    
-    
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :description)
+  end
 end
