2147b09b7a8c1202443775054816875e1abaebe6
jose queiroz <zemqueiroz@gmail.com>
2021-02-23 11:44:40 +0000

    tweaked the device authentication process to include home and index

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,6 @@
 class DogsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: :home
+
   def index
     @dogs = Dog.all
   end
