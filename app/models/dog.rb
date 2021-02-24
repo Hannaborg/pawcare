64e8604b8266028cbe1074ee07d6bc2019eca2d4
jose queiroz <zemqueiroz@gmail.com>
2021-02-24 14:50:32 +0000

    added upload picture on creating a new dog entry

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,7 @@ class Dog < ApplicationRecord
 	belongs_to :user
 	has_many :bookings, dependent: :destroy
 	has_many :booked_users, through: :bookings, source: :user
+	has_one_attached :photo
 
 	validates :name, presence: true
 	validates :breed, :description, presence: true
