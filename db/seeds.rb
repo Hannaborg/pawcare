faf760a3dd91c102049185381720f6c5f64825fe
Hanna Borg <hanna.r.borg@gmail.com>
2021-02-25 15:56:31 +0100

    small changes to the index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@ puts "creating user..."
 puts "creating dogs..."
 
 user1 = User.create(name: "Rui", email: "rui@gmail.com", password: "portugal")
-dog1 = Dog.create(name: "snoopy", breed: "terrier", description: "test 3", user_id: user1.id)
+dog1 = Dog.create(name: "Snoopy", breed: "Beagle", description: "Snoopy, comic-strip character, a spotted white beagle with a rich fantasy life. The pet dog of the hapless Peanuts character Charlie Brown, Snoopy became one of the most iconic and beloved characters in the history of comics.", user_id: user1.id)
 
-user2 = User.create(name: "Joe", email: "Joe@gmail.com", password: "england")
-dog2 = Dog.create(name: "Lucy", breed: "german Shepard", description: "Beautiful dog, really well taught", user_id: user1.id)
+user2 = User.create(name: "Joe", email: "joe@gmail.com", password: "england")
+dog2 = Dog.create(name: "Lassie", breed: "Rough Collie", description: "When most people think of a Collie, the dog that probably comes to mind is the legendary Lassie. Star of film, television, novels, cartoons, and comic strips, Lassie has become a household name and an important icon in American pop culture. These pups are very intelligent and sensitive to their owners' mood, and are great herders. ", user_id: user1.id)
 
 user3 = User.create(name: "Rita", email: "Rita@gmail.com", password: "portugal")
-dog3 = Dog.create(name: "Momo", breed: "beagle", description: "Lazy little dog but with lots of love to give", user_id: user1.id)
+dog3 = Dog.create(name: "Lady", breed: "Cocker Spaniel", description: "Lady is rather demure and naive but makes a good friend to those who know her and is very faithful to her family. She is not completely helpless it is shown that she can be brave and protective. She was not afraid of the rat and would chase it away from her home and she took on the responsibility to protect Jim Jr.", user_id: user1.id)
 
